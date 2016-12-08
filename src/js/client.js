ea122051f384880f1043430982fa9c29839faa91
Nderim <rimitopalli95@gmail.com>
2016-12-08 17:29:58 +0100

    added Provider

--- src/js/client.js
+++ src/js/client.js
@@ -6,7 +6,7 @@ import AppRoutes from './components/AppRoutes';
 import store from "./store"
 
 window.onload = () => {
-	ReactDOM.render(<AppRoutes store={store} />, document.getElementById('main'));
+	ReactDOM.render(<Provider store={store}><AppRoutes/></Provider>, document.getElementById('main'));
 }
 
 //Isomorphic applications principles
