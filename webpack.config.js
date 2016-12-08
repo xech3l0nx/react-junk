52f808c2ed8bd2d5c6ac9249b8a50b2fb1b46dbe
Alessio Cozzolino <alessio@majeeko.com>
2016-12-08 12:02:56 +0100

    new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,11 @@ const path = require('path');
 
 module.exports = {
   devtool: debug ? 'inline-sourcemap' : null,
-  entry: path.join(__dirname, 'src/js', 'client.js'),
+  entry: [
+  'webpack/hot/only-dev-server',
+  path.join(__dirname, 'src/js/client.js'),
+  'webpack-dev-server/client?localhost:8080'
+  ],
   devServer: {
     inline: true,
     contentBase: "./dist/",
@@ -20,59 +24,33 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      test: path.join(__dirname, 'src/js'),
-      loader: ['babel-loader'],
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
       query: {
-        cacheDirectory: 'babel_cache',
-        presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
+        presets: ['react', 'es2015', 'stage-0'],
+        plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
       }
-    }]
+    }
+    ]
   },
   plugins: debug ? [] : [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      mangle: true,
-      sourcemap: false,
-      beautify: false,
-      dead_code: true
-    }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false },
+    mangle: true,
+    sourcemap: false,
+    beautify: false,
+    dead_code: true
+  }),
   ]
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // var debug = process.env.NODE_ENV !== "production";
 // var webpack = require('webpack');
 // var path = require('path');
