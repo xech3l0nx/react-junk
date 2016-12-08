b7099ef7b9b0aef65f2cac1202073268c1371ed5
Alessio Cozzolino <alessio@majeeko.com>
2016-12-08 18:05:31 +0100

    renames dist folder to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const debug = process.env.NODE_ENV !== "production";
-
 const webpack = require('webpack');
 const path = require('path');
 
@@ -11,13 +10,13 @@ module.exports = {
   devServer: {
     inline: true,
     port: 3000,
-    contentBase: "dist",
+    contentBase: "public",
     historyApiFallback: {
       index: '/index-static.html'
     }
   },
   output: {
-    path: path.join(__dirname, 'dist', 'js'),
+    path: path.join(__dirname, 'public', 'js'),
     publicPath: "/js/",
     filename: 'bundle.js'
   },
@@ -49,87 +48,3 @@ module.exports = {
   }),
   ]
 };
-// "use strict";
-// const debug = process.env.NODE_ENV !== "production";
-
-// const webpack = require('webpack');
-// const path = require('path');
-
-// module.exports = {
-//   devtool: debug ? 'inline-sourcemap' : null,
-//   entry: [
-//   'webpack/hot/only-dev-server',
-//   path.join(__dirname, 'src/js/client.js'),
-//   'webpack-dev-server/client?localhost:8080'
-//   ],
-//   devServer: {
-//     inline: true,
-//     contentBase: "./dist/",
-//     historyApiFallback: {
-//       index: '/dist/index.html'
-//     }
-//   },
-//   output: {
-//     path: path.join(__dirname, 'dist/js'),
-//     publicPath: "/js/",
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     loaders: [
-//     {
-//       test: /\.jsx?$/,
-//       exclude: /(node_modules|bower_components)/,
-//       loader: 'babel-loader',
-//       query: {
-//         presets: ['react', 'es2015', 'stage-0'],
-//         plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-//       }
-//     }
-//     ]
-//   },
-//   plugins: debug ? [] : [
-//   new webpack.DefinePlugin({
-//     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-//   }),
-//   new webpack.optimize.DedupePlugin(),
-//   new webpack.optimize.OccurenceOrderPlugin(),
-//   new webpack.optimize.UglifyJsPlugin({
-//     compress: { warnings: false },
-//     mangle: true,
-//     sourcemap: false,
-//     beautify: false,
-//     dead_code: true
-//   }),
-//   ]
-// };
-// var debug = process.env.NODE_ENV !== "production";
-// var webpack = require('webpack');
-// var path = require('path');
-
-// module.exports = {
-//   context: path.join(__dirname, "src"),
-//   devtool: debug ? "inline-sourcemap" : null,
-//   entry: "./js/client.js",
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /(node_modules|bower_components)/,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['react', 'es2015', 'stage-0'],
-//           plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-//         }
-//       }
-//     ]
-//   },
-//   output: {
-//     path: path.join(__dirname, "dist/js"),
-//     filename: "bundle.js"
-//   },
-//   plugins: debug ? [] : [
-//     new webpack.optimize.DedupePlugin(),
-//     new webpack.optimize.OccurenceOrderPlugin(),
-//     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-//   ],
-// };
